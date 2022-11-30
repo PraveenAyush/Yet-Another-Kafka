4781096b2eca7147453b7a313529cd91a3aa2f4a
Ayush Praveen <ayushpr.ap@outlook.com>
2022-11-30 23:13:34 +0530

    created CLI for producer
    moved CLI code for consumer from cli.py to main

--- producer/__main__.py
+++ producer/__main__.py
@@ -1,13 +1,33 @@
 import asyncio
 
 from producer import Producer
+import argparse
 
+# Create the parser
+producer_parse = argparse.ArgumentParser(
+    prog='producer', description='Choose the producer topic')
+
+# Add the arguments
+producer_parse.add_argument('--topic', '-tp',
+                            metavar="<topic>",
+                            action='store',
+                            type=str,
+                            help='topic to publish',
+                            required=True)
+
+producer_parse.add_argument('--key', '-k',
+                            metavar="<key>",
+                            action='store',
+                            type=str,
+                            help='decides which partition to write to')
+
+args = producer_parse.parse_args()
 
 async def main() -> None:
     """Run producer server."""
-    topic = input("Enter a Topic: ")
-    key = input("Enter a key: ")
-   
+    topic = args.topic
+    key = args.key
+
     publisher = Producer('localhost',9999,topic,key)
     await publisher.setup()
 
